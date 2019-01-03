44650634d47a5d29666bb7b2eff5e8f8403ed7c4
Scytheroid <bohdan.kopcak@gmail.com>
2019-01-03 15:45:57 +0100

    Unfinished CetnostCisel added

--- src/CetnostCisel.java
+++ src/CetnostCisel.java
@@ -3,28 +3,62 @@ import java.util.*;
 public class CetnostCisel {
     public static void main(String[] args) {
 
-        java.util.Scanner sc = new java.util.Scanner(System.in);
-        List<Integer> numbers = new ArrayList<>();
-        List<Integer> occurrence = new ArrayList<>();
+        String s = "2 3 3 2 6 5";
+        java.util.Scanner vstup = new java.util.Scanner(s).useDelimiter("\\s");
+        List<Integer> cisla = new ArrayList<>();
+        List<Integer> cetnosti = new ArrayList<>();
 
+
+        // Postupne cti jednotliva cisla na vstupu
+        cteniVstupu:
+        while (vstup.hasNextInt()) {
+            int dalsiCislo = vstup.nextInt();
+            // Pokud jiz cislo nekde v listu je, zvys jeho cetnost o 1
+            for (int i = 0; i < cisla.size(); i++) {
+                if (dalsiCislo == cisla.get(i)) {
+                    cetnosti.set(i, cetnosti.get(i) + 1);
+                    // Pokud jiz cislo v listu bylo a zvysili jsme jeho cetnost,
+                    // muzeme zbytek while cyklu preskocit a pokracovat s dalsim cislem na vstupu
+                    continue cteniVstupu;
+                }
+            }
+
+            // Pokud zatim cislo v listu nebylo,
+            // pridej ho na konec listu a nastav jeho cetnost na 1
+            cisla.add(dalsiCislo);
+            cetnosti.add(1);
+        }
+
+        for (int i = 0; i < cisla.size(); i++) {
+            System.out.printf("%d ", cisla.get(i));
+            System.out.printf("%d", cetnosti.get(i));
+            System.out.printf("\n");
+        }
+    }
+
+        /*
         // Input processing
         while (sc.hasNextInt()) {
             int nextNum = sc.nextInt();
-            int found = -1;
+            boolean found = false;
             for (int i = 0; i < numbers.size(); i++) {
                 // If number is already in the list
                 if (nextNum == numbers.get(i)){
                     occurrence.set(i, occurrence.get(i) + 1);
-                    found = 1;
+                    found = true;
                     break;
                 }
             }
             // If the number was absent
-            if (found == -1) {
+            if (found) {
                 numbers.add(nextNum);
                 occurrence.add(1);
             }
         }
+
+        sc.close();
+*/
+/*
 
         for (int i = 0; i < numbers.size(); i++) {
             System.out.printf("%d ", numbers.get(i));
@@ -34,5 +68,5 @@ public class CetnostCisel {
 
     }
 
-
+*/
 }
