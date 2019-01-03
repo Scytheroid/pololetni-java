79bac883fba1145c977564f297535c0e06d4bbde
Scytheroid <bohdan.kopcak@gmail.com>
2019-01-03 15:07:43 +0100

    Unfinished CetnostCisel added

--- src/CetnostCisel.java
+++ src/CetnostCisel.java
@@ -1,28 +1,37 @@
 import java.util.*;
 
 public class CetnostCisel {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
         java.util.Scanner sc = new java.util.Scanner(System.in);
-        Map<Integer,Integer> occurrence = new HashMap<Integer, Integer>();
+        List<Integer> numbers = new ArrayList<>();
+        List<Integer> occurrence = new ArrayList<>();
 
+        // Input processing
         while (sc.hasNextInt()) {
-            occurrence.compute(sc.nextInt());
+            int nextNum = sc.nextInt();
+            int found = -1;
+            for (int i = 0; i < numbers.size(); i++) {
+                // If number is already in the list
+                if (nextNum == numbers.get(i)){
+                    occurrence.set(i, occurrence.get(i) + 1);
+                    found = 1;
+                    break;
+                }
+            }
+            // If the number was absent
+            if (found == -1) {
+                numbers.add(nextNum);
+                occurrence.add(1);
+            }
         }
 
-        System.out.println(occurrence);
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.printf("%d ", numbers.get(i));
+            System.out.printf("%d", occurrence.get(i));
+            System.out.printf("\n");
+        }
 
-        /*
-        // A thing that makes stream from map, it sorts it and makes it a map again with deltas
-        Map<Integer,Integer> sorted = cisla
-                .entrySet()
-                .stream()
-                .sorted(comparingByValue())
-                .collect(
-                        toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2, LinkedHashMap::new));
-
-        System.out.printf("Sorted: " + sorted);
-        */
     }
 
 
