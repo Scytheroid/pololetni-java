138004b1012273a040903aeca92c3771b9728fc7
Scytheroid <bohdan.kopcak@gmail.com>
2019-01-03 16:40:05 +0100

    CetnsotCisel finished and working good

--- src/CetnostCisel.java
+++ src/CetnostCisel.java
@@ -2,41 +2,10 @@ import java.util.*;
 
 public class CetnostCisel {
     public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in).useDelimiter("\\s");
+        List<Integer> numbers = new ArrayList<>();
+        List<Integer> occurrence = new ArrayList<>();
 
-        String s = "2 3 3 2 6 5";
-        java.util.Scanner vstup = new java.util.Scanner(s).useDelimiter("\\s");
-        List<Integer> cisla = new ArrayList<>();
-        List<Integer> cetnosti = new ArrayList<>();
-
-
-        // Postupne cti jednotliva cisla na vstupu
-        cteniVstupu:
-        while (vstup.hasNextInt()) {
-            int dalsiCislo = vstup.nextInt();
-            // Pokud jiz cislo nekde v listu je, zvys jeho cetnost o 1
-            for (int i = 0; i < cisla.size(); i++) {
-                if (dalsiCislo == cisla.get(i)) {
-                    cetnosti.set(i, cetnosti.get(i) + 1);
-                    // Pokud jiz cislo v listu bylo a zvysili jsme jeho cetnost,
-                    // muzeme zbytek while cyklu preskocit a pokracovat s dalsim cislem na vstupu
-                    continue cteniVstupu;
-                }
-            }
-
-            // Pokud zatim cislo v listu nebylo,
-            // pridej ho na konec listu a nastav jeho cetnost na 1
-            cisla.add(dalsiCislo);
-            cetnosti.add(1);
-        }
-
-        for (int i = 0; i < cisla.size(); i++) {
-            System.out.printf("%d ", cisla.get(i));
-            System.out.printf("%d", cetnosti.get(i));
-            System.out.printf("\n");
-        }
-    }
-
-        /*
         // Input processing
         while (sc.hasNextInt()) {
             int nextNum = sc.nextInt();
@@ -50,23 +19,39 @@ public class CetnostCisel {
                 }
             }
             // If the number was absent
-            if (found) {
+            if (!found) {
                 numbers.add(nextNum);
                 occurrence.add(1);
             }
         }
 
         sc.close();
-*/
-/*
+
+        // Sorting by size
+        boolean sorted = false;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < numbers.size() - 1; i++) {
+                if (occurrence.get(i) < occurrence.get(i + 1)){
+
+                    sorted = false;
+
+                    int tempNum = numbers.get(i);
+                    int tempOcc = occurrence.get(i);
+                    numbers.set(i, numbers.get(i + 1));
+                    numbers.set(i + 1, tempNum);
+                    occurrence.set(i, occurrence.get(i + 1));
+                    occurrence.set(i + 1, tempOcc);
+                }
+            }
+        }
+
+
 
         for (int i = 0; i < numbers.size(); i++) {
             System.out.printf("%d ", numbers.get(i));
-            System.out.printf("%d", occurrence.get(i));
-            System.out.printf("\n");
         }
 
     }
 
-*/
 }
