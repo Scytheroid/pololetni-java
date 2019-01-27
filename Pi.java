8d086067e68793f4e5c4fd1bc1706c7e2a7175b0
Scytheroid <bohdan.kopcak@gmail.com>
2019-01-27 17:29:30 +0100

    Pi maths repaired, now showing better approximation

--- Pi.java
+++ Pi.java
@@ -1,26 +1,26 @@
 import java.util.concurrent.TimeUnit;
 
 public class Pi {
-    final static int POLOMER = 1000;
+    final static int POLOMER = 10000;
 
     public static void main(String[] args)
             throws InterruptedException {
 
         int uvnitr = 0;
-        int vne = 0;
+        int celkem = 0;
         double pi;
         java.util.Random rand = new java.util.Random();
 
         while(true) {
             int x = rand.nextInt(2 * POLOMER +1);
             int y = rand.nextInt(2 * POLOMER +1);
+
+            celkem += 1;
             if (jeKruh(x, y))
                 uvnitr += 1;
-            else
-                vne += 1;
-            pi = (double) uvnitr / vne;
-            System.out.printf("%.6f\r", pi);
-            Thread.sleep(50);
+            pi = 4 * ((double) uvnitr / celkem);
+            if (uvnitr % 10000 == 0)
+                System.out.printf("%.6f\r", pi);
         }
 
     }
